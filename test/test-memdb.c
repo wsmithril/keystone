87df877b5d620e39498f9ebdbb867a33fb032418
WsMithril <WsMithril@gmail.com>
2012-12-31 16:38:26 +0800

    Update unit test for storage_mem

--- test/test-memdb.c
+++ test/test-memdb.c
@@ -6,57 +6,49 @@
 static int ks_memdb_ut(KS_MEMDB * db) {
     int i = 0;
     char k[128]; // key byffer
-    KS_MEMDB_REC * rec;
-
-#define show(rec) do { \
-    if (rec) printf("key: %s, value: %s\n", (char*)rec->key, (char*)rec->value); \
-    else printf("Not found\n"); \
-} while (0)
+    char v[128];
 
     // TEST adding 100 keys
     for (i = 0; i < 100; i++) {
         sprintf(k, "%05d", i);
         ks_memdb_add(db, k, 6, k, 6, ADDMODE_REPLACE);
-        rec = ks_memdb_lookup(db, k, 6);
+        ks_memdb_get_value(db, k, 6, v, NULL);
         // key in rec MUST be the same with k, but not the same pointer
-        if (!( (strcmp(rec->key, k) == 0)
-            && (k != rec->key)
-            && (6 == rec->sk))) {
-            printf("TEST add not pass\n");
+        if (!((0 == memcmp(v, k, 6)))) {
+            printf("TEST ADD SKIP failed\n");
             exit(1);
         }
     }
-    printf("=== ADD test pass ===\n");
-    ks_memdb_dumpdb(db);
+    printf("=== ADD SKIP test pass ===\n");
 
     // TEST adding 100 keys
     for (i = 0; i < 100; i++) {
         sprintf(k, "%05d", i);
         ks_memdb_add(db, k, 6, k, 6, ADDMODE_APPEND);
-        rec = ks_memdb_lookup(db, k, 6);
-        if (!( (strcmp(rec->key, k) == 0)
-            && (k != rec->key)
-            && (6 == rec->sk)
-            && (strncmp(k, rec->value, 5) == 0)
-            && (strcmp(k, rec->value + 5) == 0))) {
-            printf("TEST append not pass\n");
+        ks_memdb_get_value(db, k, 6, v, NULL);
+        if (!( (strncmp(k, v + 5, 5) == 0)
+            && (strcmp( k, v + 5) == 0))) {
+            printf("TEST ADD APPEND failed\n");
             exit(2);
         }
     }
-    printf("=== ADD append pass ===\n");
-    ks_memdb_dumpdb(db);
+    printf("=== ADD APPEND pass ===\n");
 
+    v[0] = 0;
     // testing delete
     for (i = 1; i < 100; i += 2) {
+        int ret = 0;
         sprintf(k, "%05d", i);
         ks_memdb_delete(db, k, 6);
-        rec = ks_memdb_lookup(db, k, 6);
-        if (rec) {
-            printf("TEST DELETE not pass\n");
+        ret = ks_memdb_get_value(db, k, 6, v, NULL);
+        if (!((v[0] == 0)
+           && (ret == 5))) {
+            printf("TEST DELETE failed, v[0] = %c, ret = %d\n", v[0], ret);
             exit(3);
         }
     }
-    printf("=== ADD append pass ===\n");
+    printf("=== DELETE pass ===\n");
+    printf("\n The whole database:\n");
     ks_memdb_dumpdb(db);
     return 0;
 }
